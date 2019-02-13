afe034391b25302ab56696d433612ad552f4c230
rosaridho <muhammadridhorosa@gmail.com>
2019-02-13 10:03:25 +0700

    first try pythonanywhere

--- project_advance_views/project_advance_views/settings.py
+++ project_advance_views/project_advance_views/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Blog',
+    'Mentee',
+    'Mentor',
+    'Home',
+    'Author',
 ]
 
 MIDDLEWARE = [
